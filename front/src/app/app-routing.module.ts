781a08fec211695fbc65604ac2fdbd1ec9d904d4
lucas albornoz <lucasalbornoz004@gmail.com>
2022-10-25 10:37:49 -0300

    Cambios en educacion

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditAbountMeComponent } from './components/abount-me/edit-abount-me.component';
 import { NewAbountMeComponent } from './components/abount-me/new-abount-me.component';
+import { NewEducationComponent } from './components/education/new-education.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -9,7 +10,9 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'createPersona', component: NewAbountMeComponent},
-  {path: 'editPersona/:id', component: EditAbountMeComponent}
+  {path: 'editPersona/:id', component: EditAbountMeComponent},
+  {path: 'nuevaEdu', component: NewEducationComponent}
+  
 ];
 
 @NgModule({
--- front/src/app/components/education/new-education.component.ts
+++ front/src/app/components/education/new-education.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/service/educacion.service';
 
 @Component({
   selector: 'app-new-education',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-education.component.css']
 })
 export class NewEducationComponent implements OnInit {
+  nombreEdu: string;
+  fechaEdu: string;
 
-  constructor() { }
+  constructor(private educacionS: EducacionService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(): void {
+    const educacion = new Educacion(this.nombreEdu, this.fechaEdu);
+    this.educacionS.save(educacion).subscribe(
+      data=>{
+        alert("Educacion añadida correctamente");
+        this.router.navigate(['']);
+      }, err => {
+        alert("fallo al agregar");
+        this.router.navigate(['']);
+      }
+      )
   }
 
 }
